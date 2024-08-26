c59ba99fc26dc75ce230d706e5cba33efb8b701c
Ksenia Lanetskaia <kseniia.lanetskaia@scr.sk>
2024-08-26 22:59:30 +0200

    Data fetching

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,56 @@
+import { type ReactNode, useEffect, useState } from 'react';
+
+import BlogPosts, { BlogPost } from './components/BlogPosts.tsx';
+import { get } from './util/http.ts';
+import fetchingImg from './assets/data-fetching.png';
+
+// The type of the raw data fetched from the API.
+type RawDataBlogPost = {
+  id: number;
+  userId: number;
+  title: string;
+  body: string;
+};
+
 function App() {
-  return <h1>Data Fetching!</h1>;
+  const [fetchedPosts, setFetchedPosts] = useState<BlogPost[]>();
+
+  useEffect(() => {
+    async function fetchPosts() {
+      // data will have the type of RawDataBlogPost[].
+      const data = (await get(
+        'https://jsonplaceholder.typicode.com/posts'
+      )) as RawDataBlogPost[];
+
+      // Transform the raw data of RawDataBlogPost type into BlogPost type.
+      const blogPosts: BlogPost[] = data.map((rawPost) => {
+        return {
+          id: rawPost.id,
+          title: rawPost.title,
+          text: rawPost.body,
+        };
+      });
+
+      // Now we can use the transformed data.
+      setFetchedPosts(blogPosts);
+    }
+
+    fetchPosts();
+  }, []);
+
+  return (
+    <main>
+      <img
+        src={fetchingImg}
+        alt="An abstract image depicting a data fetching process."
+      />
+      {fetchedPosts ? (
+        <BlogPosts posts={fetchedPosts} />
+      ) : (
+        <p>Loading...</p>
+      )}
+    </main>
+  );
 }
 
 export default App;
