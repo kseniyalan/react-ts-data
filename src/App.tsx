f56158bda177a468a1d3e3b9f9e197f58109d3d9
Ksenia Lanetskaia <kseniia.lanetskaia@scr.sk>
2024-08-26 23:19:38 +0200

    Error and Loading state

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { type ReactNode, useEffect, useState } from 'react';
 import BlogPosts, { BlogPost } from './components/BlogPosts.tsx';
 import { get } from './util/http.ts';
 import fetchingImg from './assets/data-fetching.png';
+import ErrorMessage from './components/ErrorMessage.tsx'; 
 
 // The type of the raw data fetched from the API.
 type RawDataBlogPost = {
@@ -14,30 +15,57 @@ type RawDataBlogPost = {
 
 function App() {
   const [fetchedPosts, setFetchedPosts] = useState<BlogPost[]>();
+  const [isFetching, setIsFetching] = useState(false);
+  const [error, setError] = useState<string | null>(null);
 
   useEffect(() => {
     async function fetchPosts() {
-      // get() is a generic function that can retata of any type we want.
-      // Here we provide the type RawDataBlogPost as the type argument -> so function will return data of this type.
-      const data = await get<RawDataBlogPost[]>(
-        'https://jsonplaceholder.typicode.com/posts'
-      );
+      setIsFetching(true);
+      setError(null);
+      try {
+        // get() is a generic function that can return data of any type we want.
+        // Here we provide the type RawDataBlogPost as the type argument -> so function will return data of this type.
+        const data = await get<RawDataBlogPost[]>(
+          'https://jsonplaceholder.typicode.com/posts'
+        );
 
-      // Transform the raw data of RawDataBlogPost type into BlogPost type.
-      const blogPosts: BlogPost[] = data.map((rawPost) => {
-        return {
-          id: rawPost.id,
-          title: rawPost.title,
-          text: rawPost.body,
-        };
-      });
+        // Transform the raw data of RawDataBlogPost type into BlogPost type.
+        const blogPosts: BlogPost[] = data.map((rawPost) => {
+          return {
+            id: rawPost.id,
+            title: rawPost.title,
+            text: rawPost.body,
+          };
+        });
+        setFetchedPosts(blogPosts);
+      } catch (error) {
+        // If error is the instance of JavaScript Error class, set the error message.
+        if (error instanceof Error) {
+          setError(error.message);
+        } else {
+          setError('Failed to fetch posts!');
+        } 
+      }
 
-      // Now we can use the transformed data.
-      setFetchedPosts(blogPosts);
+      setIsFetching(false);
     }
 
     fetchPosts();
   }, []);
+
+  let content: ReactNode;
+
+  if (error) {
+    content = <ErrorMessage text={error} />;
+  }
+
+  if (fetchedPosts) {
+    content = <BlogPosts posts={fetchedPosts} />;
+  }
+
+  if (isFetching) {
+    content = <p id="loading-fallback">Loading...</p>;
+  }
 
   return (
     <main>
@@ -45,11 +73,7 @@ function App() {
         src={fetchingImg}
         alt="An abstract image depicting a data fetching process."
       />
-      {fetchedPosts ? (
-        <BlogPosts posts={fetchedPosts} />
-      ) : (
-        <p>Loading...</p>
-      )}
+      {content}
     </main>
   );
 }
